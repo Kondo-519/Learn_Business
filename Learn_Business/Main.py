a707acb166212e5c752a31d980ce04528ef1a0eb
ironhill13 <ironhill13@gmail.com>
2019-09-29 12:38:33 +0900

    Fix

--- Learn_Business/Main.py
+++ Learn_Business/Main.py
@@ -3,18 +3,9 @@ from Wiki.WikiExtractorShell import WikiExtractorShell
 from MeCabShell.MeCabShell import MeCabShell
 from InputData import InputData
 from Check_LearnData import Check_LearnData
-import OutputModel
+from ModelUtil import ModelUtil
 import sys
 
-#一時的
-import gensim
-from gensim import corpora, matutils
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-import numpy as np
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 #引数check
 if len(sys.argv) < 3 :
@@ -52,91 +43,15 @@ for data in inputDataList:
 #分析その２
 #Analyzer.explore_data.plot_sample_length_distribution_inputData(inputDataList)
 
-# 学習する
-# 逆文章頻度で文章をベクトル化。文章の長さに結果が依存しないよう、この方式を採用。
-
-# DictDataをoutput
-# https://qiita.com/u6k/items/5170b8d8e3f41531f08a
-
-# extendなら、配列が入れ子にならない(入れ子はappend）
+# 文章データを配列化。
 texts = []
 for data in inputDataList:
-    texts.append(data.keywords)
+    texts.append(data.keywords) # extendなら、配列が入れ子にならない(入れ子はappend）
 
+# ラベルデータを配列化
 labels = [data.category for data in inputDataList]
 
-output = OutputModel(texts, labels)
-
-#Dcitionaryを作成(https://qiita.com/tatsuya-miyamoto/items/f505dfa8d5307f8c6e98)
-#Dcitionary.token2id : 単語/idの辞書データ
-#Dcitionary.token2id : id/df値(出現文書数)の辞書データ
-dictionary = corpora.Dictionary(texts)
-#単語データをフィルタリング。
-#no_below: 「出現文書数≥指定値」になるような語のみを保持する（一定回数以下のゴミワードを削除）。
-#no_above: 「出現文書数/全文書数≤指定値」になるような語のみを保持する（多すぎるワードを削除）。
-#num_docs : 辞書作成に用いた全文章数
-#num_pocs : 辞書作成に用いた全単語数
-dictionary.filter_extremes(no_below = 5, no_above = 0.5)
-
-#ベクトル化
-#def vec2dense(vec, num_terms):
-#    return list(matutils.corpus2dense([vec], num_terms=num_terms).T[0])
-# コーパス作成(文章ごとに「単語ID・出現頻度」タプル配列を持つデータ）し、ベクトル化
-#data_all = [vec2dense(dictionary.doc2bow(texts[i]), len(dictionary)) for i in range(len(texts))]
-
-#2次元配列を作る。
-np.set_printoptions(precision=2)
-hai = np.array(texts)
-
-#TfidfVectorizer
-vectorizer = TfidfVectorizer(analyzer=dictionary.doc2bow, use_idf=True, token_pattern=u'(?u)\\b\\w+\\b',min_df=0.05, max_df=0.8)
-vecs = vectorizer.fit_transform(hai)
-
-#テスト用データ作成
-#train_data = data_all
-train_data = vecs.toarray()
-X_train, X_test, y_train, y_test = train_test_split(train_data, labels, test_size=0.3, random_state=1)
-
-#モデル用データ作成
-
-
-#標準偏差取得
-sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
-
-
-#モデル出力#
-import pickle
-import os
-from pathlib import Path
-output_path = Path(sys.argv[2])
-
-## SVM Model##
-# https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
-from sklearn.svm import SVC
-
-
-#評価用モデル作成(rbf)
-model = SVC(kernel='rbf', gamma ='auto')
-model.fit(X_train_std, y_train)
-#精度確認
-score = model.score(X_test_std, y_test)
-print('SVC(rbf) score is {:.3g}'.format(score))
-
-#保存用モデル作成
-model.fit(train_data, labels)
-filename = 'SVM_rbf_model.sav'
-pickle.dump(model, open(os.path.join(output_path, filename), 'wb'))
-
-#評価用モデル作成(linear)
-model = SVC(kernel='linear')
-model.fit(X_train_std, y_train)
-#精度確認
-score = model.score(X_test_std, y_test)
-print('SVC(linear) score is {:.3g}'.format(score))
-
-
-print(y_test)
-print(model.predict(X_test_std))
+#モデルを作成・評価・出力
+models = ModelUtil(texts, labels, sys.argv[2])
+models.ValueAllModel()
+models.OutputAllModel()
---
+++ Learn_Business/ModelUtil.py
@@ -0,0 +1,272 @@
+import gensim
+from gensim import corpora, matutils
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+import pickle
+import os
+from pathlib import Path
+
+## Decision Tree Model ##
+from sklearn.tree import DecisionTreeClassifier
+
+## SVM Model##
+# https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
+from sklearn.svm import SVC
+
+## Random Forest Model##
+from sklearn.ensemble import RandomForestClassifier
+
+## Naive Bayes Model##
+#https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html#sklearn.naive_bayes.GaussianNB
+from sklearn.naive_bayes import GaussianNB
+
+## Logistic Regression##
+from sklearn.linear_model import LogisticRegression
+
+#評価について
+#sklearnのチートシートによれば、サンプル50以下は別方式が望ましい。
+#https://scikit-learn.org/stable/tutorial/machine_learning_map/
+#第一候補：SVM_linear
+#第二候補：ナーイブベイズ
+#この二個をベースに考える。
+
+class ModelUtil(object):
+    """
+    モデルデータを評価し、出力するクラス
+    # 逆文章頻度で文章をベクトル化。文章の長さに結果が依存しないよう、この方式を採用。
+    
+    Parameters
+
+    ----------
+
+    texts : str[][]
+        モデルデータ作成元の文章データ（分かち書き済）の２次元配列。
+
+    train_data_std : str[][]
+        モデルデータ作成元の文章データ（分かち書き済）の２次元配列。（tf-idf と 標準化済）
+
+    labels : str[]
+        文書カテゴリー情報
+
+    X_train_std : str[][]
+        評価用文章データ（標準化済）
+
+    X_test_std : str[][]
+        テスト用文章データ（標準化済）
+
+    y_train : str[]
+        評価用カテゴリーデータ
+
+    y_test : str[]
+        テスト用カテゴリーデータ
+
+    outputFolder : Path
+        出力フォルダパス
+    """
+
+    def __init__(self, texts, labels, outputPath):
+        """
+
+        Parameters
+
+        ----------
+
+        texts : str[][]
+
+            モデルデータ作成元の文章データ（分かち書き済）の２次元配列。
+
+        labels : str[]
+
+            文書カテゴリー情報
+
+        outputPath : str
+            モデル出力先のフォルダ情報
+        """
+        self.texts = texts
+        self.labels = labels
+
+        #Dcitionaryを作成(https://qiita.com/tatsuya-miyamoto/items/f505dfa8d5307f8c6e98)
+        #Dcitionary.token2id : 単語/idの辞書データ
+        #Dcitionary.token2id : id/df値(出現文書数)の辞書データ
+        dictionary = corpora.Dictionary(texts)
+
+        #単語データをフィルタリング。
+        #no_below: 「出現文書数≥指定値」になるような語のみを保持する（一定回数以下のゴミワードを削除）。
+        #no_above: 「出現文書数/全文書数≤指定値」になるような語のみを保持する（多すぎるワードを削除）。
+        #num_docs : 辞書作成に用いた全文章数
+        #num_pocs : 辞書作成に用いた全単語数
+        dictionary.filter_extremes(no_below = 5, no_above = 0.5)
+
+        #2次元配列を作る。
+        np.set_printoptions(precision=2)
+
+        #TfidfVectorizer
+        vectorizer = TfidfVectorizer(analyzer=dictionary.doc2bow, use_idf=True, token_pattern=u'(?u)\\b\\w+\\b',min_df=0.05, max_df=0.8)
+
+        #テスト用データ作成
+        train_data = vectorizer.fit_transform(np.array(texts)).toarray()
+        X_train, X_test, self.y_train, self.y_test = train_test_split(train_data, labels, test_size=0.3, random_state=1)
+
+        #標準化
+        sc = StandardScaler()
+        sc.fit(X_train)
+        self.X_train_std = sc.transform(X_train)
+        self.X_test_std = sc.transform(X_test)
+        self.train_data_std = sc.transform(train_data)
+
+        #出力先フォルダ
+        self.outputFolder = Path(outputPath)
+
+
+    def ValueSVM(self):
+        """
+        SVMモデルを評価する。
+        """
+        #評価用モデル作成(rbf)
+        model = SVC(kernel='rbf', gamma ='auto')
+        model.fit(self.X_train_std, self.y_train)
+        #精度確認
+        score = model.score(self.X_test_std, self.y_test)
+        print('SVC(rbf) score is {:.3g}'.format(score))
+
+        #評価用モデル作成(linear)
+        model = SVC(kernel='linear')
+        model.fit(self.X_train_std, self.y_train)
+        #精度確認
+        score = model.score(self.X_test_std, self.y_test)
+        print('SVC(linear) score is {:.3g}'.format(score))
+
+    def ValueDecisionTree(self):
+        """
+        Decision Treeモデルを評価する。
+        """
+        for i in range(10):
+            #評価用モデル作成(depth = i)
+            model = DecisionTreeClassifier(max_depth=i+1)
+            model.fit(self.X_train_std, self.y_train)
+
+            #精度確認
+            score = model.score(self.X_test_std, self.y_test)
+            print('desision tree of depth' + str(i+1) + ' score is {:.3g}'.format(score))
+
+    def ValueRandomForest(self):
+        """
+        RandomForestモデルを評価する。
+        """
+        for i in range(10):
+            #評価用モデル作成(depth = i)
+            model = RandomForestClassifier(max_depth=i+1, n_estimators=100)
+            model.fit(self.X_train_std, self.y_train)
+
+            #精度確認
+            score = model.score(self.X_test_std, self.y_test)
+            print('Random Forest of depth' + str(i+1) + ' score is {:.3g}'.format(score))
+
+    def ValueNaiveBayes(self):
+        """
+        Naive Bayesモデルを評価する。
+        """
+        #評価用モデル作成
+        model = GaussianNB()
+        model.fit(self.X_train_std, self.y_train)
+        #精度確認
+        score = model.score(self.X_test_std, self.y_test)
+        print('Naive Bayes score is {:.3g}'.format(score))
+
+    def ValueLogistic(self):
+        """
+        ロジスティック回帰モデルを評価する。
+        """
+        #評価用モデル作成
+        model = LogisticRegression(solver = 'lbfgs', multi_class='auto')
+        model.fit(self.X_train_std, self.y_train)
+        #精度確認
+        score = model.score(self.X_test_std, self.y_test)
+        print('Logistic score is {:.3g}'.format(score))
+
+    #######ここからOutput######
+
+    def OutputSVM(self):
+        """
+        SVMモデルを出力する。
+        """
+        model = SVC(kernel='rbf', gamma ='auto')
+        model.fit(self.train_data_std, self.labels)
+        filename = 'SVM_rbf_model.sav'
+        pickle.dump(model, open(os.path.join(self.outputFolder, filename), 'wb'))
+
+        model = SVC(kernel='linear', gamma ='auto')
+        model.fit(self.train_data_std, self.labels)
+        filename = 'SVM_linear_model.sav'
+        pickle.dump(model, open(os.path.join(self.outputFolder, filename), 'wb'))
+
+    def OutputDecisionTree(self):
+        """
+        Decision Treeモデルを出力する。
+        """
+        for i in range(10):
+            #評価用モデル作成(depth = i)
+            model = DecisionTreeClassifier(max_depth=i+1)
+            model.fit(self.train_data_std, self.labels)
+
+            filename = 'DecisionTree_mode_'+ str(i+1) +'.sav'
+            pickle.dump(model, open(os.path.join(self.outputFolder, filename), 'wb'))
+
+    def OutputRandomForest(self):
+        """
+        RandomForestモデルを出力する。
+        """
+        for i in range(10):
+            #評価用モデル作成(depth = i)
+            model = RandomForestClassifier(max_depth=i+1, n_estimators=100)
+            model.fit(self.train_data_std, self.labels)
+
+            filename = 'RandomForest_mode_'+ str(i+1) +'.sav'
+            pickle.dump(model, open(os.path.join(self.outputFolder, filename), 'wb'))
+
+    def OutputNaiveBayes(self):
+        """
+        NaiveBayesモデルを出力する。
+        """
+        model = model = GaussianNB()
+        model.fit(self.train_data_std, self.labels)
+        filename = 'NaiveBayes_model.sav'
+        pickle.dump(model, open(os.path.join(self.outputFolder, filename), 'wb'))
+
+    def OutputLogistic(self):
+        """
+        Logisticモデルを出力する。
+        """
+        model = model = LogisticRegression(solver = 'lbfgs', multi_class='auto')
+        model.fit(self.train_data_std, self.labels)
+        filename = 'Logistic_model.sav'
+        pickle.dump(model, open(os.path.join(self.outputFolder, filename), 'wb'))
+
+    #######ここからAll#######
+
+    def ValueAllModel(self):
+        """
+        全てのモデルを評価する。
+
+        """
+        self.ValueSVM()
+        self.ValueDecisionTree()
+        self.ValueRandomForest()
+        self.ValueNaiveBayes()
+        self.ValueLogistic()
+
+    def OutputAllModel(self):
+        """
+        全てのモデルを出力する。
+
+        """
+        self.OutputSVM()
+        self.OutputDecisionTree()
+        self.OutputRandomForest()
+        self.OutputNaiveBayes()
+        self.OutputLogistic()
